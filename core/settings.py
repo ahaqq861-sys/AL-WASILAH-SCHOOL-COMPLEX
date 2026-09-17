6b7ece7da11c470eda69ffcc0e7e92d41dc5727e
Abdul Haqq Dramani Jawula <ahaqq861@gmail.com>
2026-09-17 02:57:24 +0000

    Configure LOGIN_URL to portal login route

--- core/settings.py
+++ core/settings.py
@@ -96,3 +96,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_URL = '/portal/login/'
+LOGIN_REDIRECT_URL = '/portal/'
+LOGOUT_REDIRECT_URL = '/portal/login/'
