90fcfdf55b8585c28c486937e2d01423f078c11f
Abdul Haqq Dramani Jawula <ahaqq861@gmail.com>
2026-09-18 15:01:19 +0000

    Complete superuser creation and static file collection

--- portal/admin.py
+++ portal/admin.py
@@ -1,29 +1,14 @@
 from django.contrib import admin
-from .models import ClassLevel, AcademicTerm, StudentProfile, TeacherProfile, Grade, FeeRecord
+from .models import Student, Grade
 
-@admin.action(description="Promote selected students to the next class level")
-def promote_students(modeladmin, request, queryset):
-    promoted_count = 0
-    graduated_count = 0
-    for student in queryset:
-        if student.current_class and student.current_class.next_class:
-            student.current_class = student.current_class.next_class
-            student.save()
-            promoted_count += 1
-        elif student.current_class and not student.current_class.next_class:
-            student.is_graduated = True
-            student.save()
-            graduated_count += 1
-    modeladmin.message_user(request, f"Successfully promoted {promoted_count} students. {graduated_count} marked as graduated.")
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('student_id', 'name', 'student_class', 'gender', 'fees_due', 'fees_paid', 'promoted')
+    search_fields = ('student_id', 'name', 'student_class')
+    list_filter = ('student_class', 'gender', 'promoted')
 
-@admin.register(StudentProfile)
-class StudentProfileAdmin(admin.ModelAdmin):
-    list_display = ('student_id', 'user', 'current_class', 'is_graduated')
-    list_filter = ('current_class', 'is_graduated')
-    actions = [promote_students]
-
-admin.site.register(ClassLevel)
-admin.site.register(AcademicTerm)
-admin.site.register(TeacherProfile)
-admin.site.register(Grade)
-admin.site.register(FeeRecord)
+@admin.register(Grade)
+class GradeAdmin(admin.ModelAdmin):
+    list_display = ('student', 'subject', 'trimester', 'class_score', 'exam_score', 'total_score', 'grade')
+    search_fields = ('student__name', 'student__student_id', 'subject')
+    list_filter = ('trimester', 'subject', 'grade')
