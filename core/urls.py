69aa4d7c53175fd422a00660eeafbb149cb3bd0d
Abdul Haqq Dramani Jawula <ahaqq861@gmail.com>
2026-09-21 16:06:21 +0000

    Fix model field renames and apply portal migrations

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('portal/', include('portal.urls', namespace='portal')),
-    path('', include('portal.urls')),  # Directs the root URL (/) straight to the portal
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- portal/urls.py
+++ portal/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('register/', views.register_user, name='register_user'),
     path('fee-ledger/<int:ledger_id>/edit/', views.edit_fee_ledger, name='edit_fee_ledger'),
-    path('portal/', include('portal.urls')),
 ]
